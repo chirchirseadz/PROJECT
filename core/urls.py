98ef49a818084257c9aea16396c5292903217089
kipchi <sydneychirchir@gmail.com>
2022-09-28 14:06:42 +0300

    first initial project commit

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 """core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,26 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from . import settings
-
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-from users import views as user_views
-from django.contrib.auth import views as auth_views 
+from django.urls import path
 
 urlpatterns = [
-    # administartion 
     path('admin/', admin.site.urls),
-    path('', include('dashboard.urls')),
-
-    # users authentications 
-    path('register/', user_views.register, name='signup'),
-    path('login/', auth_views.LoginView.as_view(template_name = ('users/login.html')), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name = ('users/logout.html')), name='logout'),
-
-
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- users/views.py
+++ users/views.py
@@ -1,21 +1,3 @@
-from django.shortcuts import render, redirect
-from sympy import Si
-from . forms import SignUpForm
-from django.contrib import messages
-from django.contrib.auth.decorators import login_required
-# Create your views here. 
+from django.shortcuts import render
 
-def register(request):
-    if request.method == 'POST':
-        user_form = SignUpForm(request.POST)
-        if user_form.is_valid():
-            user_form.save()
-            return redirect('landing_page')
-    else:
-        user_form = SignUpForm()
-    context = {
-        'user_form': user_form
-    }
-    return render(request, 'users/signup.html', context)
-
-
+# Create your views here.
