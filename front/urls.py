3d00daa4ff3abe49a455684911aa7a1bf4e22054
kipchi <sydneychirchir@gmail.com>
2023-01-16 14:21:50 +0300

    second commit

--- front/urls.py
+++ front/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.index, name='index_page'),
     path('landing', views.landing_page, name='landing_page'),
     path('predictions/results', views.results_page, name='results_page'),
-    path('/visualizations/', views.visualizations, name='visualizations'),
+    path('visualizations/', views.visualizations, name='visualizations'),
 
 
     
